6005ef8267cde1dbb6ad4e8e55cf99ff6b19783d
René Zurita Corro <zc.rene@gmail.com>
2013-09-26 13:15:16 -0500

    first commit: custom install and upgrade script.

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -1,6 +1,7 @@
 printf '\033[0;34m%s\033[0m\n' "Upgrading Oh My Zsh"
 cd "$ZSH"
-if git pull --rebase --stat origin master
+
+if {git pull --rebase --stat origin master && git pull --rebase --stat origin upstream}
 then
   printf '\033[0;32m%s\033[0m\n' '         __                                     __   '
   printf '\033[0;32m%s\033[0m\n' '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
@@ -13,4 +14,3 @@ then
 else
   printf '\033[0;31m%s\033[0m\n' 'There was an error updating. Try again later?'
 fi
-
